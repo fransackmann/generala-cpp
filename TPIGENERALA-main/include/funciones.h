b6190873bb9fd7ee7b10add2213d7caa23377f7e
fransackmann <fransackmann@gmail.com>
2026-01-28 17:05:13 -0300

    Add files via upload

---
+++ TPIGENERALA-main/include/funciones.h
@@ -0,0 +1,29 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+#include <iostream>
+
+using namespace std;
+
+/// MENU Y PANTALLA
+void mostrarMenu();
+void limpiarPantalla();
+void mostrarCreditos();
+
+/// MODO UN JUGADOR
+void jugarUnJugador(int &puntajeTotal, string &nombre, string &maxJugador, int &maxPuntaje);
+
+/// MODO DOS JUGADORES
+void jugarDosJugadores(string &maxJugador, int &maxPuntaje);
+void jugarTurno(int &puntajeTotal, string nombre, string &maxJugador, int &maxPuntaje);
+
+/// MECANICAS DEL JUEGO
+void tirarDados(int dados[], int cantidad, bool mManual);
+void mostrarDados(int dados[], int cantidad, int numLanzamiento);
+void contarDados(int dados[], int conteo[]);
+void combinaciones(int conteo[], int &puntajeRonda, int numLanzamiento);
+
+/// PUNTAJE
+void cargarPuntaje (string nombre, int puntajeTotal, string &maxJugador, int &maxPuntaje);
+void mostrarPuntaje(string maxJugador, int maxPuntaje);
+
+#endif
---
+++ TPIGENERALA-main/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include "funciones.h"
+#include <cstdlib>
+#include <ctime>
+#include "rlutil.h"
+using namespace rlutil;
+using namespace std;
+
+int main(){
+
+    srand(time(NULL)); ///PARA QUE RAND GENERE NUMEROS ALEATORIOS TODAS LAS PARTIDAS
+
+    int opcion;
+    int puntajeTotal = 0;
+
+    string nombre;
+    string maxJugador = "";
+
+    int maxPuntaje = 0;
+
+    ///MOSTRAR MENU
+    do{
+        mostrarMenu();
+
+        gotoxy(45,17);
+        cout << "- SELECCIONE UNA OPCION: " << endl;
+        cin >> opcion;
+
+        cls();
+
+        switch(opcion){
+            case 1: {
+                /// PARTIDA UN JUGADOR
+                jugarUnJugador(puntajeTotal, nombre, maxJugador, maxPuntaje);
+                break;
+            }
+            case 2:
+                /// PARTIDA DOS JUGADORES
+                jugarDosJugadores(maxJugador, maxPuntaje);
+                break;
+            case 3:
+                /// VER PUNTAJE MAXIMO
+                mostrarPuntaje(maxJugador, maxPuntaje);
+                break;
+            case 4:
+                /// VER CREDITOS
+                mostrarCreditos();
+                break;
+            case 5:
+                /// SALIR
+                cout << "Saliendo del juego..." << endl;
+                break;
+            default:
+                /// OPCION NO VALIDA
+                cout << "Opcion no valida, intentalo de nuevo" << endl;
+                limpiarPantalla();
+                break;
+        }
+
+    }while(opcion!=5);
+
+    return 0;
+}
